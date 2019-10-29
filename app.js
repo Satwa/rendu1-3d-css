18c9bc160669baad1e64cabf29c6483c82bade5a
Joshua <satwa.fr@gmail.com>
2019-10-29 12:18:31 +0100

    add timing w/ btn

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 /******/ document.querySelector(".machine_door_container").classList.toggle("is-open")
 
+const timeScreenElement = document.querySelector(".time_screen")
 
 
 
 document.querySelector(".door_opener").addEventListener("click", () => {
     document.querySelector(".machine_door_container").classList.toggle("is-open")
+})
+
+document.querySelectorAll(".time_selector div:not(.time_selector_empty)").forEach((btn) => {
+    btn.addEventListener("click", function() {
+        if(this.innerText !== "<-"){
+            timeScreenElement.innerText += this.innerText
+        }else{
+            timeScreenElement.innerText = timeScreenElement.innerText.slice(0, timeScreenElement.innerText.length - 1)
+        }
+    })
 })
